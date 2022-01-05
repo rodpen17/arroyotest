09b06c858fc0828aa50a7755ed1e5e8f5bf91cd7
rodrigo <rodrigo@inspr.com>
2022-01-04 18:40:38 -0600

    added office dependency

--- Chocolatey.ps1
+++ Chocolatey.ps1
@@ -1,4 +1,4 @@
 #Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install nodejs javaruntime jdk11 git 
+choco install nodejs javaruntime jdk11 git office365business
